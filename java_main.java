988fb08b8d39e506a19dfd3646aa9e730d87beda
Khushboo Singh <41840494+khushboosinghrecs@users.noreply.github.com>
2020-12-06 20:38:51 +0530

    Create java_main.java

---
+++ java_main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws IOException
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String s1[]=br.readLine().split(" ");
+		int t=Integer.parseInt(s1[0]);
+		for(int j=0;j<t;j++){
+		    String s2[]=br.readLine().split(" ");
+		    int n=Integer.parseInt(s2[0]);
+		    int d=Integer.parseInt(s2[1]);
+		    int arr[]=new int[n];
+		    int c=0;
+		    String s3[]=br.readLine().split(" ");
+		    for(int i=0;i<n;i++)
+		    arr[j]=Integer.parseInt(s3[j]);
+        }
+        }
+        }
